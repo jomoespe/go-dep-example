556f9f29f33b7e42d4331bf03e8cee56336050c4
Jose <jomoespe@gmail.com>
2017-04-25 23:17:46 +0200

    Extract constants

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+// A shell-style sample application
 package main
 
 import (
@@ -6,8 +7,13 @@ import (
 	"github.com/chzyer/readline"
 )
 
+const (
+	prompt   = "go-dep-example> "
+	exit_cmd = "exit"
+)
+
 func main() {
-	rl, err := readline.New("go-dep-example> ")
+	rl, err := readline.New(prompt)
 	if err != nil {
 		panic(err)
 	}
@@ -15,7 +21,7 @@ func main() {
 
 	for {
 		line, err := rl.Readline()
-		if err != nil || line == "exit" {
+		if err != nil || line == exit_cmd {
 			break
 		}
 		fmt.Printf("received command: %s\n", line)
